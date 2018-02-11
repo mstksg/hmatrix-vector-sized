ee674ff79ee0e923f44cc869421874ced3de47b8
Justin Le <justin@jle.im>
2018-02-10 21:06:59 -0800

    complex matrix operations

--- src/Numeric/LinearAlgebra/Static/Vector.hs
+++ src/Numeric/LinearAlgebra/Static/Vector.hs
@@ -1,90 +1,159 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+-- |
+-- Module      : Numeric.LinearAgelbra.Static.Vector
+-- Copyright   : (c) Justin Le 2018
+-- License     : BSD3
+--
+-- Maintainer  : justin@jle.im
+-- Stability   : experimental
+-- Portability : non-portable
+--
+-- Conversions between statically sized types in
+-- "Numeric.LinearAlgebra.Static" from /hmatrix/ and /vector-sized/.
+--
+-- This module is intentionally minimal, exporting only functions that
+-- cannot be written without "unsafe" operations.  With these, however, you
+-- can easy write other useful combinators by using type-safe operations
+-- like 'fmap', 'VS.map', 'liftA2', etc.
+--
+
 module Numeric.LinearAlgebra.Static.Vector (
+  -- * Vector
+  -- ** Real
     rVec
   , vecR
+  -- ** Complex
   , cVec
   , vecC
+  -- * Matrix
+  -- ** Real
   , lRows
   , rowsL
   , lCols
   , colsL
+  -- ** Complex
+  , mRows
+  , rowsM
+  , mCols
+  , colsM
   ) where
 
 import           Data.Foldable
-import           Data.Maybe
-import           GHC.TypeLits
-import           Text.Printf
+import           Unsafe.Coerce
+import qualified Data.Vector                  as UV
 import qualified Data.Vector.Sized            as V
-import qualified Data.Vector.Storable         as UVS
 import qualified Data.Vector.Storable.Sized   as VS
+import qualified Numeric.LinearAlgebra        as HU
 import qualified Numeric.LinearAlgebra.Static as H
 
-rVec :: KnownNat n => H.R n -> VS.Vector n H.ℝ
-rVec x = fromMaybe (error e) $ VS.toSized y
-  where
-    y = H.extract x
-    e = printf "rVec: Unexpected size mismatch (expected %d, got %d)"
-          (H.size x) (UVS.length y)
+-- | Convert an /hmatrix/ vector (parameterized by its lenth) to
+-- a /vector-sized/ storable vector of 'Double's.
+rVec :: H.R n -> VS.Vector n H.ℝ
+rVec = unsafeCoerce
 
-vecR :: KnownNat n => VS.Vector n H.ℝ -> H.R n
-vecR x = fromMaybe (error e) $ H.create y
-  where
-    y = VS.fromSized x
-    e = printf "vecR: Unexpected size mismatch (expected %d, got %d)"
-          (VS.length x) (UVS.length y)
+-- | Convert a /vector-sized/ storable vector to an /hmatrix/ vector
+-- (parameterized by its lenth).
+vecR :: VS.Vector n H.ℝ -> H.R n
+vecR = unsafeCoerce
 
-cVec :: KnownNat n => H.C n -> VS.Vector n H.ℂ
-cVec x = fromMaybe (error e) $ VS.toSized y
-  where
-    y = H.extract x
-    e = printf "rVec: Unexpected size mismatch (expected %d, got %d)"
-          (H.size x) (UVS.length y)
+-- | Convert an /hmatrix/ complex vector (parameterized by its lenth) to
+-- a /vector-sized/ storable vector of 'Complex Double's, preserving the
+-- length in the type.
+cVec :: H.C n -> VS.Vector n H.ℂ
+cVec = unsafeCoerce
 
-vecC :: KnownNat n => VS.Vector n H.ℂ -> H.C n
-vecC x = fromMaybe (error e) $ H.create y
-  where
-    y = VS.fromSized x
-    e = printf "vecR: Unexpected size mismatch (expected %d, got %d)"
-          (VS.length x) (UVS.length y)
+-- | Convert a /vector-sized/ storable vector to an /hmatrix/ complex
+-- vector (parameterized by its lenth), preserving the length in the type.
+vecC :: VS.Vector n H.ℂ -> H.C n
+vecC = unsafeCoerce
 
+-- | Split an /hmatrix/ matrix (parameterized by its dimensions) to
+-- a /vector-sized/ boxed vector of its rows (as /hmatrix/ vectors).
 lRows
-    :: (KnownNat m, KnownNat n)
+    :: forall m n. ()
     => H.L m n
     -> V.Vector m (H.R n)
-lRows x = fromMaybe (error e) $ V.fromList y
-  where
-    y = H.toRows x
-    e = printf "lRows: Unexpected length mismatch (expected %d, got %d)"
-          (fst (H.size x)) (length y)
+lRows = unsafeCoerce
+      . UV.fromList
+      . HU.toRows
+      . (unsafeCoerce :: H.L m n -> HU.Matrix Double)
 
+-- | Join together a /vector-sized/ boxed vector of /hmatrix/ vectors to an
+-- /hmatrix/ matrix as its rows.
 rowsL
-    :: (KnownNat m, KnownNat n)
+    :: forall m n. ()
     => V.Vector m (H.R n)
     -> H.L m n
-rowsL x = H.withRows y $ \z ->
-            fromMaybe (error e) (H.exactDims z)
-  where
-    y = toList x
-    e = printf "rowsL: Unexpected length mismatch (expected %d, got %d)"
-          (V.length x) (length y)
+rowsL = (unsafeCoerce :: HU.Matrix Double -> H.L m n)
+      . HU.fromRows
+      . map (unsafeCoerce :: H.R n -> HU.Vector Double)
+      . toList
 
+-- | Split an /hmatrix/ matrix (parameterized by its dimensions) to
+-- a /vector-sized/ boxed vector of its columns (as /hmatrix/ vectors).
 lCols
-    :: (KnownNat m, KnownNat n)
+    :: forall m n. ()
     => H.L m n
     -> V.Vector n (H.R m)
-lCols x = fromMaybe (error e) $ V.fromList y
-  where
-    y = H.toColumns x
-    e = printf "lCols: Unexpected length mismatch (expected %d, got %d)"
-          (snd (H.size x)) (length y)
+lCols = unsafeCoerce
+      . UV.fromList
+      . HU.toColumns
+      . (unsafeCoerce :: H.L m n -> HU.Matrix Double)
 
+-- | Join together a /vector-sized/ boxed vector of /hmatrix/ vectors to an
+-- /hmatrix/ matrix as its columns.
 colsL
-    :: (KnownNat m, KnownNat n)
+    :: forall m n. ()
     => V.Vector n (H.R m)
     -> H.L m n
-colsL x = H.withColumns y $ \z ->
-            fromMaybe (error e) (H.exactDims z)
-  where
-    y = toList x
-    e = printf "colsL: Unexpected length mismatch (expected %d, got %d)"
-          (V.length x) (length y)
+colsL = (unsafeCoerce :: HU.Matrix Double -> H.L m n)
+      . HU.fromColumns
+      . map (unsafeCoerce :: H.R m -> HU.Vector Double)
+      . toList
 
+-- | Split an /hmatrix/ complex matrix (parameterized by its dimensions) to
+-- a /vector-sized/ boxed vector of its rows (as /hmatrix/ complex
+-- vectors).
+mRows
+    :: forall m n. ()
+    => H.M m n
+    -> V.Vector m (H.C n)
+mRows = unsafeCoerce
+      . UV.fromList
+      . HU.toRows
+      . (unsafeCoerce :: H.M m n -> HU.Matrix H.ℂ)
+
+-- | Join together a /vector-sized/ boxed vector of /hmatrix/ complex
+-- vectors to an /hmatrix/ complex matrix as its rows.
+rowsM
+    :: forall m n. ()
+    => V.Vector m (H.C n)
+    -> H.M m n
+rowsM = (unsafeCoerce :: HU.Matrix H.ℂ -> H.M m n)
+      . HU.fromRows
+      . map (unsafeCoerce :: H.C n -> HU.Vector H.ℂ)
+      . toList
+
+-- | Split an /hmatrix/ complex matrix (parameterized by its dimensions) to
+-- a /vector-sized/ boxed vector of its columns (as /hmatrix/ complex
+-- vectors).
+mCols
+    :: forall m n. ()
+    => H.M m n
+    -> V.Vector n (H.C m)
+mCols = unsafeCoerce
+      . UV.fromList
+      . HU.toColumns
+      . (unsafeCoerce :: H.M m n -> HU.Matrix H.ℂ)
+
+-- | Join together a /vector-sized/ boxed vector of /hmatrix/ complex
+-- vectors to an /hmatrix/ complex matrix as its columns.
+colsM
+    :: forall m n. ()
+    => V.Vector n (H.C m)
+    -> H.M m n
+colsM = (unsafeCoerce :: HU.Matrix H.ℂ -> H.M m n)
+      . HU.fromColumns
+      . map (unsafeCoerce :: H.C m -> HU.Vector H.ℂ)
+      . toList
--- test/Spec.hs
+++ test/Spec.hs
@@ -17,49 +17,49 @@ import qualified Data.Vector.Storable                as UVS
 import qualified Data.Vector.Storable.Sized          as VS
 import qualified Hedgehog.Gen                        as Gen
 import qualified Hedgehog.Range                      as Range
+import qualified Numeric.LinearAlgebra               as HU
 import qualified Numeric.LinearAlgebra.Static        as H
 import qualified Numeric.LinearAlgebra.Static.Vector as H
 
+genDouble :: Gen H.ℝ
+genDouble = Gen.double (Range.linearFracFrom 0 (-10) 10)
+
+genComplex :: Gen H.ℂ
+genComplex = (:+) <$> genDouble <*> genDouble
+
 prop_rVec :: Property
 prop_rVec = property $ do
-    xs <- forAll $ Gen.list (Range.constant 5 10)
-                            (Gen.double (Range.linearFracFrom 0 (-10) 10))
+    xs <- forAll $ Gen.list (Range.constant 5 10) genDouble
     case fromJust $ someNatVal (fromIntegral (length xs)) of
       SomeNat (Proxy :: Proxy n) ->
-        tripping xs (H.rVec . H.vector @n)
-                    (Identity . UVS.toList . H.extract . H.vecR)
+        tripping xs ((* 2) . H.rVec . H.vector @n)
+                    (Identity . UVS.toList . H.extract . (/ 2) . H.vecR)
 
 prop_vecR :: Property
 prop_vecR = property $ do
-    xs <- forAll $ Gen.list (Range.constant 5 10)
-                            (Gen.double (Range.linearFracFrom 0 (-10) 10))
+    xs <- forAll $ Gen.list (Range.constant 5 10) genDouble
     VS.withSizedList xs $ \v ->
-      tripping v H.vecR (Identity . H.rVec)
-
-genComplex :: Gen H.ℂ
-genComplex = (:+) <$> Gen.double (Range.linearFracFrom 0 (-10) 10)
-                  <*> Gen.double (Range.linearFracFrom 0 (-10) 10)
+      tripping v ((* 2) . H.vecR) (Identity . (/ 2) . H.rVec)
 
 prop_cVec :: Property
 prop_cVec = property $ do
     xs <- forAll $ Gen.list (Range.constant 5 10) genComplex
     case fromJust $ someNatVal (fromIntegral (length xs)) of
       SomeNat (Proxy :: Proxy n) ->
-        tripping xs (H.cVec @n . fromJust . H.create . UVS.fromList)
-                    (Identity . UVS.toList . H.extract . H.vecC)
+        tripping xs ((* 2) . H.cVec @n . fromJust . H.create . UVS.fromList)
+                    (Identity . UVS.toList . H.extract . (/ 2) . H.vecC)
 
 prop_vecC :: Property
 prop_vecC = property $ do
     xs <- forAll $ Gen.list (Range.constant 5 10) genComplex
     VS.withSizedList xs $ \v ->
-      tripping v H.vecC (Identity . H.cVec)
+      tripping v ((* 2) . H.vecC) (Identity . (/ 2) . H.cVec)
 
-genMatList :: Gen (SomeNat, SomeNat, [[Double]])
-genMatList = do
+genMatList :: Gen a -> Gen (SomeNat, SomeNat, [[a]])
+genMatList g = do
     m  <- Gen.int (Range.constant 5 10)
     n  <- Gen.int (Range.constant 5 10)
-    xs <- (replicateM m . replicateM n) $
-      Gen.double (Range.linearFracFrom 0 (-10) 10)
+    xs <- (replicateM m . replicateM n) g
     return ( fromJust . someNatVal . fromIntegral $ m
            , fromJust . someNatVal . fromIntegral $ n
            , xs
@@ -70,32 +70,70 @@ prop_lRows = property $ do
     ( SomeNat (Proxy :: Proxy m)
      , SomeNat (Proxy :: Proxy n)
      , xs
-     ) <- forAll genMatList
-    tripping xs (H.lRows @m @n . H.matrix . concat)
-                (Identity . map (UVS.toList . H.extract) . V.toList)
+     ) <- forAll $ genMatList genDouble
+    tripping xs ((* 2) . H.lRows @m @n . H.matrix . concat)
+                (Identity . map (UVS.toList . H.extract) . H.toRows . (/ 2) . H.rowsL)
+
+prop_rowsL :: Property
+prop_rowsL = property $ do
+    (_, SomeNat (Proxy :: Proxy n), xs)  <- forAll $ genMatList genDouble
+    V.withSizedList xs $ \v ->
+      tripping v ((* 2) . H.rowsL . V.map (H.vector @n))
+                 (Identity . V.map (UVS.toList . H.extract . (/ 2)) . H.lRows)
 
 prop_lCols :: Property
 prop_lCols = property $ do
     ( SomeNat (Proxy :: Proxy m)
      , SomeNat (Proxy :: Proxy n)
      , xs
-     ) <- forAll genMatList
-    tripping xs (H.lCols @m @n . H.matrix . concat)
-                (Identity . transpose . map (UVS.toList . H.extract) . V.toList)
-
-prop_rowsL :: Property
-prop_rowsL = property $ do
-    (_, SomeNat (Proxy :: Proxy n), xs)  <- forAll genMatList
-    V.withSizedList xs $ \v ->
-      tripping v (H.rowsL . V.map (H.vector @n))
-                 (Identity . V.map (UVS.toList . H.extract) . H.lRows)
+     ) <- forAll $ genMatList genDouble
+    tripping xs ((* 2) . H.lCols @m @n . H.matrix . concat)
+                (Identity . transpose . map (UVS.toList . H.extract) . H.toColumns . (/ 2) . H.colsL)
 
 prop_colsL :: Property
 prop_colsL = property $ do
-    (_, SomeNat (Proxy :: Proxy n), xs)  <- forAll genMatList
+    (_, SomeNat (Proxy :: Proxy n), xs)  <- forAll $ genMatList genDouble
     V.withSizedList xs $ \v ->
-      tripping v (H.colsL . V.map (H.vector @n))
-                 (Identity . V.map (UVS.toList . H.extract) . H.lCols)
+      tripping v ((* 2) . H.colsL . V.map (H.vector @n))
+                 (Identity . V.map (UVS.toList . H.extract . (/ 2)) . H.lCols)
+
+prop_mRows :: Property
+prop_mRows = property $ do
+    ( SomeNat (Proxy :: Proxy m)
+     , SomeNat (Proxy :: Proxy n)
+     , xs
+     ) <- forAll $ genMatList genComplex
+    tripping xs ((* 2) . H.mRows @m @n . fromJust . H.create . HU.fromLists)
+                (Identity . map UVS.toList . HU.toRows . H.extract . (/ 2) . H.rowsM)
+
+prop_rowsM :: Property
+prop_rowsM = property $ do
+    ( SomeNat (Proxy :: Proxy m)
+     , SomeNat (Proxy :: Proxy n)
+     , xs
+     )  <- forAll $ genMatList genComplex
+    V.withSizedList xs $ \v ->
+      tripping v ((* 2) . H.rowsM . V.map (fromJust @(H.C n) . H.create . HU.fromList))
+                 (Identity . V.map (UVS.toList @H.ℂ . H.extract . (/ 2)) . H.mRows)
+
+prop_mCols :: Property
+prop_mCols = property $ do
+    ( SomeNat (Proxy :: Proxy m)
+     , SomeNat (Proxy :: Proxy n)
+     , xs
+     ) <- forAll $ genMatList genComplex
+    tripping xs ((* 2) . H.mCols @m @n . fromJust . H.create . HU.fromLists)
+                (Identity . map UVS.toList . HU.toRows . H.extract . (/ 2) . H.colsM)
+
+prop_colsM :: Property
+prop_colsM = property $ do
+    ( SomeNat (Proxy :: Proxy m)
+     , SomeNat (Proxy :: Proxy n)
+     , xs
+     )  <- forAll $ genMatList genComplex
+    V.withSizedList xs $ \v ->
+      tripping v ((* 2) . H.colsM . V.map (fromJust @(H.C n) . H.create . HU.fromList))
+                 (Identity . V.map (UVS.toList @H.ℂ . H.extract . (/ 2)) . H.mCols)
 
 main :: IO ()
 main = do
